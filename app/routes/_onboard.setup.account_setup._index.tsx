551d3e8839c8283c36a144d773a6caafd72a54f4
Jacob Kwasi Bordes <kwasibordesjacob@gmail.com>
2024-02-28 11:42:00 +0000

    onboarding: account setup, school info setup

--- app/routes/_onboard.setup.account_setup._index.tsx
+++ app/routes/_onboard.setup.account_setup._index.tsx
@@ -4,41 +4,95 @@ import {
   type MetaFunction,
   type LoaderFunction,
 } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
+import { Input, Button } from "@nextui-org/react";
 
 import ClientSetupController from "~/controllers/ClientSetupController";
 import { validateEmail, validatePassword } from "~/validators";
 
 export default function SetupProfile() {
+  const actionData = useActionData();
+
   return (
-    <section className="w-1/2 mx-auto">
-      <p className="font-bold text-2xl mb-3">User Profile</p>
+    <section className="flex flex-col gap-4 md:gap-10 h-full">
+      <div>
+        <p className="font-nunito text-violet-500 mb-3">Step 1/3</p>
+        <h2 className="font-bold font-montserrat dark:text-white text-slate-800 text-3xl md:text-5xl lg:text-6xl">
+          User Account Setup
+        </h2>
+      </div>
 
-      {/* <Form method="POST" className="flex flex-col gap-3">
-        <div className="grid w-full items-center gap-1.5">
-          <Label htmlFor="username">Your Name</Label>
-          <Input id="username" name="username" type="text" />
+      <Form
+        method="post"
+        className=" flex-1 flex flex-col md:pb-10 justify-between h-full"
+      >
+        <div className="flex flex-col md:gap-10">
+          <div className="grid grid-cols-2 gap-4 md:gap-6">
+            <Input
+              name="firstName"
+              label="First Name"
+              required
+              variant="underlined"
+              className="font-nunito"
+            />
+            <Input
+              name="lastName"
+              label="Last Name"
+              required
+              variant="underlined"
+              className="font-nunito"
+            />
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 md:gap-6">
+            <Input
+              name="email"
+              label="Email"
+              type="email"
+              required
+              variant="underlined"
+              className="font-nunito"
+              isInvalid={actionData?.errors?.email ? true : false}
+              errorMessage={actionData?.errors?.email}
+            />
+            <Input
+              name="phone"
+              label="Phone"
+              type="tel"
+              variant="underlined"
+              className="font-nunito"
+            />
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 md:gap-6">
+            <Input
+              name="password"
+              label="Password"
+              type="password"
+              required
+              variant="underlined"
+              className="font-nunito"
+              isInvalid={actionData?.errors?.password ? true : false}
+              errorMessage={actionData?.errors?.password}
+            />
+            <Input
+              name="confrimPassword"
+              label="Confirm Password"
+              type="password"
+              required
+              variant="underlined"
+              className="font-nunito"
+            />
+          </div>
         </div>
-
-        <div className="grid w-full items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" name="email" type="email" />
-        </div>
-
-        <div className="grid w-full items-center gap-1.5">
-          <Label htmlFor="phone">Phone Number</Label>
-          <Input id="phone" name="phone" type="text" />
-        </div>
-
-        <div className="grid w-full items-center gap-1.5">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" name="password" type="password" />
-        </div>
-
-        <Button type="submit" className="mt-2">
-          Proceed
+        <Button
+          type="submit"
+          className="h-12 font-montserrat md:text-lg w-1/2 bg-lightgreen hover:bg-lightgreen/80 text-slate-800"
+          variant="flat"
+        >
+          Continue
         </Button>
-      </Form> */}
+      </Form>
     </section>
   );
 }
@@ -84,7 +138,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "ComClo - Setup Account" },
+    { title: "Schoolify - Setup Account" },
     {
       name: "description",
       content: "The best e-Commerce platform for your business.",
