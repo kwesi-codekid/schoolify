2689705f6ca44a3e8d4599b1945ac4c9a99b26fb
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2024-03-04 11:19:47 +0000

    mmore

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
 export default function Index() {
   return (
     <div>
@@ -39,3 +32,35 @@ export default function Index() {
     </div>
   );
 }
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Schoolify" },
+    {
+      name: "description",
+      content:
+        "Marryright is a platform developed to educate people on Ghanaian marriages. It provides the procedures to follow to legally register one's marriage and also assist prospective couples to access relevant materials and institutions on marriage.",
+    },
+    {
+      name: "author",
+      content: "CodeKid & KwaminaWhyte",
+    },
+    { name: "og:title", content: "Schoolify" },
+    {
+      name: "og:description",
+      content:
+        "Marryright is a platform developed to educate people on Ghanaian marriages. It provides the procedures to follow to legally register one's marriage and also assist prospective couples to access relevant materials and institutions on marriage.",
+    },
+    {
+      name: "og:image",
+      content:
+        "https://res.cloudinary.com/app-deity/image/upload/v1701282976/qfdbysyu0wqeugtcq9wq.jpg",
+    },
+    { name: "og:url", content: "https://marry-right.vercel.app" },
+    {
+      name: "keywords",
+      content:
+        "legal marriages in Ghana, Pastors to bless marriages, Is he/she married?, marriiage under ordinance, cases related to marriages in Ghana, mohammedans, ordinance, traditional, verify my marriage certificate, churches legally certified to bless marriages",
+    },
+  ];
+};
