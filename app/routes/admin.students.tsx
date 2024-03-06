06f25220121fe6f79b51463c6553f887e145b890
Jacob Kwasi Bordes <kwasibordesjacob@gmail.com>
2024-03-06 10:54:41 +0000

    pull

--- app/routes/admin.students.tsx
+++ app/routes/admin.students.tsx
@@ -1,14 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import CustomTable from "~/components/custom/CustomTable";
 import CustomInput from "~/components/custom/CustomInput";
 import CustomSelect from "~/components/custom/CustomSelect";
 import CustomDatePicker from "~/components/custom/CustomDatepicker";
 
+import { useAsyncList } from "@react-stately/data";
+import { EyeOutlined } from "~/assets/icons/EyeOutlined";
+import { EditIcon } from "~/assets/icons/EditIcon";
+import { DeleteIcon } from "~/assets/icons/DeleteIcon";
+import { PlusIcon } from "~/assets/icons/PlusIcon";
+
 import AdminLayout from "~/layouts/AdminLayout";
 import { useEffect, useState } from "react";
 import { ActionFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
 import AdminController from "~/controllers/AdminController";
+import emptyFolderSVG from "~/assets/svgs/empty_folder.svg";
 import StudentController from "~/controllers/StudentController";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useNavigate } from "@remix-run/react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Spinner,
+  Tooltip,
+  useDisclosure,
+  Button,
+  Input,
+  Pagination,
+} from "@nextui-org/react";
 
 const AdminStudentsManagement = () => {
   const { students, totalPages, search_term, user, page } = useLoaderData();
@@ -145,6 +168,40 @@ const AdminStudentsManagement = () => {
 
   const [editRecord, setEditRecord] = useState(null);
 
+  // navigation logic
+  const navigate = useNavigate();
+
+  // table data:: useAsync logic, loading states
+  const [isLoading, setIsLoading] = useState(true);
+  const list = useAsyncList({
+    async load() {
+      setIsLoading(false);
+
+      return {
+        items: studentData,
+      };
+    },
+    async sort({ items, sortDescriptor }) {
+      return {
+        items: items.sort((a, b) => {
+          const first = a[sortDescriptor.column];
+          const second = b[sortDescriptor.column];
+          let cmp =
+            (parseInt(first) || first) < (parseInt(second) || second) ? -1 : 1;
+
+          if (sortDescriptor.direction === "descending") {
+            cmp *= -1;
+          }
+
+          return cmp;
+        }),
+      };
+    },
+  });
+  useEffect(() => {
+    list.reload();
+  }, [studentData, list]);
+
   return (
     <AdminLayout pageTitle="Student Management">
       <section className="p-4 backdrop-blur-[1px]">
@@ -157,6 +214,80 @@ const AdminStudentsManagement = () => {
           editRecord={editRecord}
           setEditRecord={setEditRecord}
         />
+
+        {/* <Table
+          aria-label="Students Table"
+          sortDescriptor={list.sortDescriptor}
+          onSortChange={list.sort}
+          isHeaderSticky
+          bottomContent={
+            totalPages > 0 ? (
+              <div className="flex w-full items-center">
+                <Pagination
+                  showControls
+                  showShadow
+                  color="primary"
+                  page={page}
+                  total={totalPages}
+                  onChange={(page) => {
+                    let baseUrl = location.pathname + location.search;
+                    const regex = /([?&]page=)\d+/g;
+
+                    if (
+                      baseUrl.includes("?page=") ||
+                      baseUrl.includes("&page=")
+                    ) {
+                      baseUrl = baseUrl.replace(regex, `$1${page}`);
+                    } else {
+                      baseUrl += baseUrl.includes("?")
+                        ? `&page=${page}`
+                        : `?page=${page}`;
+                    }
+
+                    navigate(baseUrl);
+                  }}
+                />
+              </div>
+            ) : null
+          }
+        >
+          <TableHeader className="!bg-blue-500">
+            {columns.map((column) => (
+              <TableColumn
+                className="font-montserrat bg-slate-900"
+                key={column.key}
+                allowsSorting
+              >
+                {column.name}
+              </TableColumn>
+            ))}
+          </TableHeader>
+          <TableBody
+            items={list.items}
+            isLoading={isLoading}
+            loadingContent={<Spinner label="Loading..." />}
+            emptyContent={
+              isLoading ? (
+                <></>
+              ) : (
+                <div className="flex items-center justify-center flex-col gap-3">
+                  <img src={emptyFolderSVG} alt="No data" />
+                  <p className="font-nunito text-lg md:text-xl">
+                    No records found
+                  </p>
+                </div>
+              )
+            }
+          >
+            {studentData.map((student: any, index: number) => (
+              <TableRow key={index}>
+                {(columnKey) => (
+                  <TableCell>{getKeyValue(student, columnKey)}</TableCell>
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table> */}
       </section>
     </AdminLayout>
   );
